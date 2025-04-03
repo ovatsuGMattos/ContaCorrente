2b48cc52447bee3b6ccbeead90d947d89237b55b
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-04-02 22:57:20 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Saque: Permite a retirada de valores, respeitando o limite máximo permitido.
+
+            //Depósito: Possibilita a adição de fundos à conta.
+
+            //Consulta de saldo: Fornece informações atualizadas sobre o montante disponível.
+
+            //Emissão de extrato: Lista todas as movimentações realizadas em um período específico.
+
+            //Transferência entre contas: Permite a movimentação de valores entre contas correntes distintas.
+
+            Console.ReadLine();
+
+        }
+    }
+}
